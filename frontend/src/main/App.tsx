c934d2138c20334ae4010c7194695daa38b6cde4
Felipe Cesar Maccari <felipecesarmaccari@gmail.com>
2025-10-07 21:33:58 -0300

    refactor(core): remove app folder

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router';
 import { Login } from '../pages/Login/Login';
-import UsuariosPage from '@/app/usuario/page';
 import { Dashboard } from '@/pages/Dashboard/Dashboard';
 import { ProtectedRoute } from '@/components/ProtectedRoute';
 import { PublicRoute } from '@/components/PublicRoute';
 import { MainLayout } from '@/components/MainLayout';
+import ListarUsuarios from '@/pages/Usuario/ListarUsuario';
 
 function App() {
   return (
@@ -32,7 +32,7 @@ function App() {
         />
         <Route
           path='usuarios'
-          element={<UsuariosPage />}
+          element={<ListarUsuarios />}
         />
       </Route>
     </Routes>
