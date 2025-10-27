ee76386dabe4cd2801c89ffc73aefddd35229653
Eduardo Ranzan Ferreira <99151894+EduardoRanzan@users.noreply.github.com>
2025-10-27 09:54:09 -0300

    Simplify main.tsx by removing unused providers
    Removed QueryClientProvider and Toaster from main render.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,9 @@ import { createRoot } from 'react-dom/client';
 import App from './main/App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'sonner';
-
-const queryClient = new QueryClient()
-
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-      <Toaster richColors position="top-right" />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
