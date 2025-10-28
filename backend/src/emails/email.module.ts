6a2d6faba5a54b30e0e30869966b18c9d8997930
Mateus Felipe de Abreu Bringhenti <lipeabreu621@gmail.com>
2025-10-27 22:45:38 -0300

    Feat (esqueceuasenha) adição dos hooks, authcontroller, prisma e emai… (#23)
    * Feat (esqueceuasenha) adição dos hooks, authcontroller, prisma e email serrvice
    
    * chore(core): remove unused dependency
    
    * Merge 'develop' into feature/esqueceu-senha
    
    * chore(core): update package lock
    
    ---------
    
    Co-authored-by: Felipe Cesar Maccari <felipecesarmaccari@gmail.com>

---
+++ backend/src/emails/email.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from 'src/services/email.service';
+
+
+@Module({
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}
