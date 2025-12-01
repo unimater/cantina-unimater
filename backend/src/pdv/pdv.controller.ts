15cb0302e785b99fe5509f20a3a79085ab54dca8
Marcelo Junior <115588060+MarceloSavitskiJunior@users.noreply.github.com>
2025-12-01 18:02:55 -0300

    [FIX] - UserId changed to be passed on guard (#45)

--- backend/src/pdv/pdv.controller.ts
+++ backend/src/pdv/pdv.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, HttpCode, UseGuards } from '@nestjs/common';
 import { PdvService } from './pdv.service';
 import { ConcluirVendaPdvDTO } from './dto/concluir-venda-pdv.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import type { UserAuthPayload } from 'src/auth/jwt.strategy';
+import { CurrentUser } from 'src/auth/current-user-decorator';
 
 @Controller('pdv')
+@UseGuards(JwtAuthGuard)
 export class PdvController {
   constructor(private readonly pdvService: PdvService) {}
 
   @Post()
   @HttpCode(201)
-  concluirVendaPdv(@Body() concluirVendaPdvDto: ConcluirVendaPdvDTO) {
-    return this.pdvService.concluirVendaPdv(concluirVendaPdvDto);
+  concluirVendaPdv(@Body() concluirVendaPdvDto: ConcluirVendaPdvDTO, @CurrentUser() user: UserAuthPayload) {
+    return this.pdvService.concluirVendaPdv(concluirVendaPdvDto, user.sub);
   }
 
 }
